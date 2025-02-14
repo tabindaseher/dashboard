35989da089793933bea30ab4fb6e2b61d9565fb4
tabindaseher <seherqureshi0602@gmail.com>
2025-02-15 00:09:04 +0500

    add use client at the top

--- app/components/products/page.tsx
+++ app/components/products/page.tsx
@@ -1,4 +1,5 @@
-import { Router, useRouter } from "next/router"
+'use client'
+import { useRouter } from "next/router"
 import { useEffect } from "react"
 
 export default function ProductedRoute ({children}:{children : React.ReactNode}){
